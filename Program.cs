fb6c17aabb895c8e51f94b473b9ab40f13e87b3e
ruchika <ruchika6698@gmail.com>
2020-04-01 10:36:37 +0530

    Added:Welcome to employee Wages

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages Computation");
+        }
+    }
+}
