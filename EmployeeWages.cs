348e06ceecf85dc89cdd4b488919937fff9ffa20
ruchika <ruchika6698@gmail.com>
2020-04-01 12:22:13 +0530

    Added:Calculate Monthly Wages

--- EmployeeWages.cs
+++ EmployeeWages.cs
@@ -6,7 +6,17 @@ namespace EmployeeWages
 {
     class EmployeeWages
     {
-        int PRESENT = 0;
+        public static int WAGE_PER_HOUR = 20;
+        public static int FULL_TIME_HOUR = 8;
+        public static int PART_TIME_HOUR = 4;
+        public static int NO_WAGE = 0;
+        private int PRESENT = 0;
+        private int MONTHLY_DAYS = 20;
+
+        private static bool isEmployeePresent;
+        private static int wageForDay;
+        private static int workHours;
+        private static int empType;
         public bool employeeAttendance()
             {
                 Random Number= new Random();
@@ -17,6 +27,45 @@ namespace EmployeeWages
         {
            return workingHours* Wage;
         }
-            
+
+
+        public int monthlyWage()
+        {
+            int employeeMonthlyWage = 0;
+            int daysWorkedInMonth = 0;
+            while (daysWorkedInMonth < MONTHLY_DAYS)
+            {
+                //Employee Present or Absent
+                isEmployeePresent = this.employeeAttendance();
+                if (isEmployeePresent == true)
+                {
+                    Random Number = new Random();
+                    empType = Number.Next(0, 2);
+                    switch (empType)
+                    {
+                        case 1:
+                            workHours = FULL_TIME_HOUR;
+                            break;
+                        case 2:
+                            workHours = PART_TIME_HOUR;
+                            break;
+                        default:
+                            Console.WriteLine("Wrong choice!");
+                            break;
+                    }
+                    //dailyWage
+                    wageForDay = emp.dailyEmployeeWages(workHours, WAGE_PER_HOUR);
+                }
+                else
+                {
+                    wageForDay = NO_WAGE;
+                }
+            }
+            // Caclulating monthly wage
+            employeeMonthlyWage += wageForDay;  
+            daysWorkedInMonth++;
+        }
+            return employeeMonthlyWage;
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,45 +4,10 @@ namespace EmployeeWages
 {
     class Program
     {
-        public static int WAGE_PER_HOUR = 20;
-        public static int FULL_TIME_HOUR = 8;
-        public static int PART_TIME_HOUR = 4;
-        public static int NO_WAGE = 0;
-
-        private static bool isEmployeePresent;
-        private static int wageForDay;
-        private static int workHours;
-        private static int empType;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation");
-            bool isEmployeePresent;
             Employee emp = new Employee();
-            isEmployeePresent = emp.employeeAttendance();
-            if( isEmployeePresent == true )
-            {
-                Random Number = new Random();
-                empType = Number.Next(0, 2);
-                switch (empType)
-                {
-                    case 1:
-                        workHours = FULL_TIME_HOUR;
-                        break;
-                    case 2:
-                        workHours = PART_TIME_HOUR;
-                        break;
-                    default:
-                        Console.WriteLine("Wrong choice!");
-                        break;
-                }
-                wageForDay = emp.dailyEmployeeWages(workHours, WAGE_PER_HOUR);
-            }
-            else
-            {
-                wageForDay = NO_WAGE;
-            }
-
-            Console.WriteLine($"{isEmployeePresent}, {wageForDay}");
         }
     }
 
